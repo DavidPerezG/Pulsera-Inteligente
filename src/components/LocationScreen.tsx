e7ab795a22ec992f5c70597fb011f0e644e68156
adriangerardo <adrian_gerardo16sch@hotmail.com>
2023-02-28 03:19:53 -0700

    Localización usuario

--- src/components/LocationScreen.tsx
+++ src/components/LocationScreen.tsx
@@ -1,25 +1,68 @@
-import React from 'react';
-import { Dimensions, Text, View, StyleSheet } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { Dimensions, Text, View, StyleSheet, ActivityIndicator, TouchableOpacity } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
-
+import * as Location from 'expo-location';
+import { Alert } from 'react-native';
+import { useFocusEffect } from '@react-navigation/native';
 
 const LocationScreen = () => {
+  const [position, setPosition] = useState(null);
+  //Obtener la locación del usuario
+  useFocusEffect(
+    React.useCallback(() => {
+      setPosition(null);
+      encontrarLocación();
+      console.log('los huevos de pascua de san andreas en particular, tienen la facultad de ser, obscuros, y muy, tetricos. *tin*')
+    },[])
+  );
+
+    async function encontrarLocación(){
+      setPosition(null);
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        Alert.alert('Aviso', 'Se deben proporcionar los permisos necesarios para utilizar la geolocalización', [
+          {text: 'OK', onPress: () => console.log('OK Pressed')},
+        ]);
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setPosition(location);
+      console.log(location);
+    };
+
   return (
     <View style={{ display: 'flex', width: '100%' }}>
-      <MapView
-        style={styles.map}
-        initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
-      >
-        <Marker
-          coordinate={{ latitude: 37.78825, longitude: -122.4324 }}
-          title={'Localización'}
-        />
-      </MapView>
+      {
+        position !== null ? (
+          <><MapView
+            style={styles.map}
+            showsMyLocationButton={true}
+            showsCompass={true}
+            scrollEnabled={true}
+            zoomEnabled={true}
+            pitchEnabled={true}
+            rotateEnabled={true}
+            initialRegion={{
+              latitude: position.coords.latitude,
+              longitude: position.coords.longitude,
+              latitudeDelta: 0.0922,
+              longitudeDelta: 0.0421
+            }}
+          >
+            <Marker
+              title='Yor are here'
+              description='This is a description'
+              coordinate={{ latitude: position.coords.latitude, longitude: position.coords.longitude }} />
+          </MapView>
+            <TouchableOpacity style={styles.buttonCallout} onPress={()=> encontrarLocación()}>
+              <Text>Actualizar Ubicación</Text>
+            </TouchableOpacity></>
+        ):
+          <View style={styles.container}>
+            <ActivityIndicator size={'large'} />
+          </View>
+      }
     </View>
   )
 }
@@ -28,6 +71,27 @@ const styles = StyleSheet.create({
   map: {
     width: Dimensions.get('window').width,
     height: Dimensions.get('window').height,
+  },
+  container: {
+    flex: 1,
+    marginTop:'70%',
+    justifyContent: 'center',
+    alignItems:'center'
+  },
+  buttonCallout: {
+    flex: 1,
+    flexDirection:'row',
+    position:'absolute',
+    top: '70%', //for center align
+    alignSelf: "center",
+    justifyContent: "center",
+    backgroundColor: "#27ee45",
+    width:250,
+    height:30,
+    textAlign: "center",
+    borderRadius: 5,
+    borderColor: "#1c337c",
+    borderWidth: 3,
   },
 });
 
